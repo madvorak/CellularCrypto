c72f49a7664b75b0fc5b2a0492f030c90aa1da18
Martin Dvořák <dvorakmartinbridge@seznam.cz>
2016-12-29 10:28:43 +0100

    Provedl jsem pár formálních úprav.

--- MartinDvorak/Program.cs
+++ MartinDvorak/Program.cs
@@ -13,8 +13,8 @@ class Program
     static void Main(string[] args)
     {
         rnd = new Random();
-        GeneticTest.RunTest();
         MainTests.RunTest();
+        GeneticTest.RunTest();
         ElementaryImplementationsTest.RunTest();
         ElementaryTimeMeasure.RunTest();
         Adapter.DisplayForm();
